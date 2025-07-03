644b145820307e3b66a11ca239b3248c624d043c
Sathish Kumar S <sk1241245@gmail.com>
2025-07-03 16:25:43 +0530

    update readme

--- src/config/config.js
+++ src/config/config.js
@@ -1,19 +1,9 @@
-// Sathish
-// const URL = "http://192.168.126.166:3001";
-
-// sathiya
-// const URL = "http://192.168.31.217:3001";
-// const URL = "http://192.168.200.44:3001";
-// const URL = "http://192.168.100.165:3001";
-
-const URL = "https://c2dapi.xdrtech.com";
+const URL = "https://your_server_url.com";
 
 
 const Config = {
     API_URL: `${URL}/api`,
-    GOOGLE_MAPS_API_KEY: "your_google_maps_api_key",
     IMAGE_BASE_URL: URL,
-    OLA_MAPS_API: 'Dtu2IjziFDwPH8Ndv34sndg2qyAOSa3IVHzg96eW'
 };
 
 export default Config;
